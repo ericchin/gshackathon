6787724de37e034a9c0062564147535d380df132
Tyler Wong <tylerwong.j@gmail.com>
2017-01-18 15:52:02 -0800

    hackathon-ui added sample replicon table to the portlet

--- modules/replicon-portlet/src/main/java/com/liferay/gs/hackathon/portlet/RepliconPortlet.java
+++ modules/replicon-portlet/src/main/java/com/liferay/gs/hackathon/portlet/RepliconPortlet.java
@@ -11,7 +11,8 @@ import org.osgi.service.component.annotations.Component;
 	property = {
 		"com.liferay.portlet.display-category=category.sample",
 		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=replicon-portlet Portlet",
+		"com.liferay.portlet.header-portlet-css=/main.css",
+		"javax.portlet.display-name=Replicon",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.resource-bundle=content.Language",
