6bdc55a7f49b1c058d14ae330badeb644a711661
Eric Chin <eric.chin@liferay.com>
2017-01-19 09:48:25 -0800

    Update rest extender with users method

--- modules/replicon/replicon-rest-extender/src/main/java/com/liferay/gshackathon/RepliconUpdateAction.java
+++ modules/replicon/replicon-rest-extender/src/main/java/com/liferay/gshackathon/RepliconUpdateAction.java
@@ -1,5 +1,12 @@
 package com.liferay.gshackathon;
 
+import com.liferay.gs.hackathon.service.RepliconService;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.service.UserLocalService;
+
 import java.util.Collections;
 import java.util.Set;
 
@@ -11,12 +18,11 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-
+/**
+ * @author Thiago Moreira
+ */
 @ApplicationPath("/replicon")
 @Component(
 	immediate = true, property = {"jaxrs.application=true"},
@@ -24,34 +30,74 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 )
 public class RepliconUpdateAction extends Application {
 
-	private static final Log log = LogFactoryUtil.getLog(RepliconUpdateAction.class);
-
 	@Override
 	public Set<Object> getSingletons() {
 		return Collections.singleton((Object)this);
 	}
 
+	@Path("/users")
 	@POST
+	@Produces("application/json")
+	public String getUsersCount(String json) throws Exception {
+		JSONObject request = JSONFactoryUtil.createJSONObject(json);
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Receiving request: \n" + request.toString(2));
+		}
+
+		int userCount = _userLocalService.getUsersCount();
+
+		JSONObject response = _generateResponse(
+			"There are " + userCount + " users in the system.");
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Sending response: \n" + response.toString(2));
+		}
+
+		return response.toJSONString();
+	}
+
+	@GET
+	@Path("/test")
+	@Produces("application/json")
+	public String test() {
+		System.out.println("testing");
+		return "{response: \"Updated, thank you for being so kind and " +
+			"responsible\"}";
+	}
+
 	@Path("/update")
+	@POST
 	@Produces("application/json")
 	public String update(String json) throws Exception {
 		JSONObject request = JSONFactoryUtil.createJSONObject(json);
 
-		if (log.isDebugEnabled()) {
-			log.debug("Receiving request: \n" + request.toString(2));
+		if (_log.isDebugEnabled()) {
+			_log.debug("Receiving request: \n" + request.toString(2));
 		}
 
+		JSONObject response = _generateResponse(
+			"Updated, thank you for being so kind and responsible");
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Sending response: \n" + response.toString(2));
+		}
+
+		return response.toJSONString();
+	}
+
+	private JSONObject _generateResponse(String message) {
 		JSONObject response = JSONFactoryUtil.createJSONObject();
 
-		response.put("version", "1.0");
 		response.put("sessionAttributes", JSONFactoryUtil.createJSONArray());
+		response.put("version", "1.0");
 
 		JSONObject responseJson = JSONFactoryUtil.createJSONObject();
 
 		JSONObject outputSpeechJson = JSONFactoryUtil.createJSONObject();
 
+		outputSpeechJson.put("text", message);
 		outputSpeechJson.put("type", "PlainText");
-		outputSpeechJson.put("text", "Updated, thank you for being so kind and responsable");
 
 		responseJson.put("outputSpeech", outputSpeechJson);
 
@@ -59,8 +105,8 @@ public class RepliconUpdateAction extends Application {
 
 		outputSpeechJson = JSONFactoryUtil.createJSONObject();
 
-		outputSpeechJson.put("type", "PlainText");
 		outputSpeechJson.put("text", (String)null);
+		outputSpeechJson.put("type", "PlainText");
 
 		repromptJson.put("outputSpeech", outputSpeechJson);
 
@@ -70,17 +116,16 @@ public class RepliconUpdateAction extends Application {
 
 		response.put("shouldEndSession", true);
 
-		if (log.isDebugEnabled()) {
-			log.debug("Sending response: \n" + response.toString(2));
-		}
-		return response.toJSONString();
+		return response;
 	}
 
-	@GET
-	@Path("/test")
-	@Produces("application/json")
-	public String test() {
-		System.out.println("testing");
-		return "{response: \"Updated, thank you for being so kind and responsable\"}";
-	}
+	private static final Log _log = LogFactoryUtil.getLog(
+		RepliconUpdateAction.class);
+
+	@Reference
+	private RepliconService _repliconService;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
 }
